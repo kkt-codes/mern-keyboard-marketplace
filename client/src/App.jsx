fb2cdcd9c53f1b68bcd446037296e295282d56f4
Kidus Ketema <kiddusketema@gmail.com>
2026-02-19 08:05:27 +0300

    chore(client): setup tailwind v4 and remove vite boilerplate

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-3xl font-bold underline">
+        Let's shop mechanical keyboards!
+      </h1>
+    </div>
+  )
+}
+
+export default App
